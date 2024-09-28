c8edf500db69aa5b05fcd07043bde92c6663c726
MatsueSS <147305790+MatsueSS@users.noreply.github.com>
2024-09-28 21:15:44 +0300

    Update graphs.c

--- graphs.c
+++ graphs.c
@@ -1,63 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
+#define SIZE 6
 
-const int length = 3;
-
-struct Node{
-    int count;
-    char name[100];
-    struct Node* array[length];
-};
-
-struct Graph{
-    int count;
-    struct Node* array[length];
-};
-
-struct Graph* __init_Graph(struct Graph* graph)
+void get_random_edge(int matrix[SIZE][SIZE])
 {
-    if(graph == NULL) { graph = (struct Graph*)malloc(sizeof(struct Graph)); graph->count = 0; }
-    return graph;
-}
-
-struct Node* __init_Node(struct Node* node, const char* nname)
-{
-    if(node == NULL) { node = (struct Node*)malloc(sizeof(struct Node)); node->count = 0; strcpy(node->name, nname); }
-    return node;
-}
-
-void add_node(struct Graph* graph, struct Node* node)
-{
-    if(graph == NULL) __init_Graph(graph);
-    if(graph->count <= length) graph->array[graph->count++] = node;
-}
-
-void add_edge(struct Node* node1, struct Node* node2)
-{
-    if(node1 != NULL && node2 != NULL)
-    {
-        node1->array[node1->count++] = node2;
-        node2->array[node2->count++] = node1;
+    srand(time(NULL));
+    for(int i = 0; i < SIZE; i++){
+        matrix[i][i] = 0;
+        for(int j = i + 1; j < SIZE; j++){
+            matrix[i][j] = (rand()%2)*(rand()%20);
+            matrix[j][i] = matrix[i][j];
+        }
     }
 }
 
-int main(void)
+void get_user_edge(int matrix[SIZE][SIZE])
 {
-    struct Graph* graph = NULL;
-    graph = __init_Graph(graph);
+    int temp;
+    for(int i = 0; i < SIZE; i++){
+        matrix[i][i] = 0;
+        for(int j = i + 1; j < SIZE; j++){
+            printf("Введите расстояние %d - %d:", i + 1, j + 1);
+            scanf("%d", &temp);
+            matrix[i][j] = temp;
+            matrix[j][i] = matrix[i][j];
+        }
+    }
+}
 
-    struct Node *node1 = NULL, *node2 = NULL, *node3 = NULL;
-    node1 = __init_Node(node1, "Первая вершина");
-    node2 = __init_Node(node2, "Вторая вершина");
-    node3 = __init_Node(node3, "Третья вершина");
+void get_matrix(int matrix[SIZE][SIZE])
+{
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    add_node(graph, node1);
-    add_node(graph, node2);
-    add_node(graph, node3);
+void dijkstra(int matrix[SIZE][SIZE], int d[SIZE], int v[SIZE])
+{
+    int minIndex, min, temp, beginIndex = 0;
+    for (int i = 0; i<SIZE; i++)
+    {
+        d[i] = 10000;
+        v[i] = 1;
+    }
+    d[beginIndex] = 0;
+    // Шаг алгоритма
+    do {
+        minIndex = 10000;
+        min = 10000;
+        for (int i = 0; i<SIZE; i++)
+        { // Если вершину ещё не обошли и вес меньше min
+            if ((v[i] == 1) && (d[i]<min))
+            { // Переприсваиваем значения
+                min = d[i];
+                minIndex = i;
+            }
+        }
+        // Добавляем найденный минимальный вес
+        // к текущему весу вершины
+        // и сравниваем с текущим минимальным весом вершины
+        if (minIndex != 10000)
+        {
+            for (int i = 0; i<SIZE; i++)
+            {
+                if (matrix[minIndex][i] > 0)
+                {
+                    temp = min + matrix[minIndex][i];
+                    if (temp < d[i])
+                    {
+                        d[i] = temp;
+                    }
+                }
+            }
+            v[minIndex] = 0;
+        }
+    } while (minIndex < 10000);
+}
 
-    add_edge(node1, node2);
-    add_edge(node2, node3);  
+int main()
+{
+    int a[SIZE][SIZE]; // матрица связей
+    int d[SIZE]; // минимальное расстояние
+    int v[SIZE]; // посещенные вершины
+    int temp;
+    // Инициализация матрицы связей
+    get_random_edge(a);
+    // Вывод матрицы связей
+    get_matrix(a);
+    //Инициализация вершин и расстояний
+    dijkstra(a, d, v);
+    // Вывод кратчайших расстояний до вершин
+    printf("\nКратчайшие расстояния до вершин: \n");
+    for (int i = 0; i<SIZE; i++)
+        printf("%5d ", d[i]);
 
-    return 0;  
+    return 0;
 }
